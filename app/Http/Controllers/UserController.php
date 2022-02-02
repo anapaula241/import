49d3412ba628271554a66bbe734a6b620eb0a202
anapaula241 <napaula241@yahoo.com.br>
2022-02-02 13:24:45 -0300

    commit inicial

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Maatwebsite\Excel\Facades\Excel;
+use App\Imports\UsersImport;
+
+class UserController extends Controller
+{
+
+    public function import()
+    {
+       return view('file-import');
+    }
+
+    /**
+    * @return \Illuminate\Support\Collection
+    */
+    public function fileImport(Request $request)
+    {
+
+        Excel::import(new UsersImport, request()->file('file'));
+
+        return back()->with('success', 'Dados foram Importados com Sucesso .');
+    }
+}
---
+++ resources/views/file-import.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Import Export Excel & CSV to Database in Laravel 7</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+</head>
+
+<body>
+    <div class="container mt-5">
+        <div class="card bg-light mt-5">
+        <div class="card-header">
+            <strong> <h4>Importar Dados</h4></strong>
+        </div>
+        <div class="card-body">
+        <form action="{{ route('file-import') }}" method="POST" enctype="multipart/form-data">
+                @csrf
+
+                @if (count($errors) > 0)
+                <div class="row">
+                    <div class="col-md-12 col-md-offset-1">
+                      <div class="alert alert-danger alert-dismissible">
+                          <button type="button" class="close" data-dismiss="alert" aria-hidden="true"></button>
+                          <h4><i class="icon fa fa-ban"></i> Error!</h4>
+                          @foreach($errors->all() as $error)
+                          {{ $error }} <br>
+                          @endforeach
+                      </div>
+                    </div>
+                </div>
+                @endif
+
+                @if (Session::has('success'))
+                    <div class="row">
+                      <div class="col-md-12 col-md-offset-1">
+                        <div class="alert alert-success alert-dismissible">
+                            <button type="button" class="close" data-dismiss="alert" aria-hidden="true"></button>
+                            <h5>{!! Session::get('success') !!}</h5>
+                        </div>
+                      </div>
+                    </div>
+                @endif
+
+                <input type="file" name="file" class="form-control">
+                <br>
+                <button class="btn btn-success">Importar Dados</button>
+        </form>
+        </div>
+    </div>
+</div>
+</body>
+
+</html>
